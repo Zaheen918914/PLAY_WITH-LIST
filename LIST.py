687af4695690971bee5a11c4cedc0aa7f0d0e43e
_ROXINE_ <zaheensirajee@gmail.com>
2024-09-18 22:30:39 +0600

    Add files via upload

---
+++ LIST.py
@@ -0,0 +1,28 @@
+frineds = ['Tahmid','Ryan','Sadid','Zaheen','Atiya']
+age = [14,14,15,16,14]
+Class = [8,6,8,8,6]
+BAZAR_ER_FORDO = ['mula','alu','gajor','morich','tomato','ada','roshun','peaaj']
+#print(frineds[0])
+#print(frineds[0:2])
+frineds.insert(0,'adib')
+#print(frineds)
+frineds.remove('Ryan')
+#print(frineds)
+#frineds.clear()
+#print(frineds)
+age.sort()
+#print(age)
+
+print(len(frineds))
+
+
+
+
+
+
+
+
+
+
+
+
